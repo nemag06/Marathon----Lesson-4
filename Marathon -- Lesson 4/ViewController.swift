6bd9891b2b9f5fe67fc3a8a808ec6b0c7bd54281
Magomet Bekov <magafif57@gmail.com>
2023-03-10 13:49:39 +0400

    Initial Commit

---
+++ Marathon -- Lesson 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marathon -- Lesson 4
+//
+//  Created by Magomet Bekov on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
